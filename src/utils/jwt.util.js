a0c8a567477258eda9f1e59b506bd8cd1ac48a2f
NewUserName <ramanpal7228@gmail.com>
2025-10-05 16:40:40 +0530

    updated

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -1,5 +1,1 @@
-import jwt from 'jsonwebtoken';
 
-const SECRET = process.env.JWT_SECRET || 'abc123';
-export const sign = (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '12h' });
-export const verify = (token) => jwt.verify(token, process.env.JWT_SECRET);
