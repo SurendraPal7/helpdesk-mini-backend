52d4f02f3340c1e208808d24fdc4937f9fe9dd93
NewUserName <ramanpal7228@gmail.com>
2025-10-05 18:49:47 +0530

    yhi

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -1,10 +1,11 @@
+// utils/jwt.util.js
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const SECRET = process.env.JWT_SECRET;
-if (!SECRET) throw new Error("JWT_SECRET is not defined in .env");
+if (!SECRET) throw new Error("JWT_SECRET not defined in .env");
 
 export const sign = (payload) => jwt.sign(payload, SECRET, { expiresIn: '12h' });
 export const verify = (token) => jwt.verify(token, SECRET);
